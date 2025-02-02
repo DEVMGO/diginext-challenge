52021f1a3fc5cb31a04b82958689924e802adba5
Mohammad Golmohammadi <92379327+DEVMGO@users.noreply.github.com>
2025-02-02 09:03:28 +0330

    edit useFilter

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,28 +1,82 @@
+import { useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import FormType from "../constants/FormType";
 
-export const EQUAL_SIGN = "~";
-export const AND_SIGN = "+";
+export const EQUAL = "~";
+export const AND = "+";
 export const ARRAY_SEPARATOR = "--";
 
-function parseUrl(url) {}
+const parseUrl = (url) => {
+  const queryString = url.split("?")[1];
+  if (!queryString) return {};
 
-function stringifyUrl(data) {}
+  return queryString.split(AND).reduce((acc, pair) => {
+    const [key, value] = pair.split(EQUAL);
+    acc[key] =
+      key === "seller-type" || key === "brand"
+        ? value.split(ARRAY_SEPARATOR).map(canBeParsedAsInt)
+        : canBeParsedAsInt(value);
 
-// TODO: complete this hook
-function useFilter(formData) {
-  console.log(formData);
+    return acc;
+  }, {});
+};
+
+const stringifyUrl = (data) => {
+  return (
+    "?" +
+    Object.entries(data)
+      .map(([key, value]) => {
+        console.log(value);
+        return Array.isArray(value)
+          ? `${key}${EQUAL}${value.join(ARRAY_SEPARATOR)}`
+          : `${key}${EQUAL}${value}`;
+      })
+      .join(AND)
+  );
+};
+
+const canBeParsedAsInt = (value) => {
+  const num = Number(value);
+  return Number.isInteger(num) && !isNaN(num) ? num : value;
+};
+
+const useFilter = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const filterState = {};
+  const filterState = useMemo(
+    () => parseUrl(location.search),
+    [location.search]
+  );
 
-  const setFilterState = (s) => {};
-  function onChange(e, name, type) {}
+  const setFilterState = (newState) => {
+    navigate(stringifyUrl(newState));
+    console.log(newState);
+  };
 
-  function onClear(name) {}
-  function onClearAll() {}
+  const onChange = (e, name, type) => {
+    const value = canBeParsedAsInt(e.target.value);
+    const newFilterState = { ...filterState };
+
+    if (type === "checkbox-group") {
+      const checkbox = filterState[name];
+      newFilterState[name] =
+        checkbox && !checkbox.includes(value) ? [...checkbox, value] : [value];
+    } else {
+      newFilterState[name] = value;
+    }
+
+    setFilterState(newFilterState);
+  };
+
+  const onClear = (name) => {
+    const newFilterState = { ...filterState };
+    delete newFilterState[name];
+    setFilterState(newFilterState);
+  };
+
+  const onClearAll = () => setFilterState({});
+
   return { filterState, setFilterState, onChange, onClear, onClearAll };
-}
+};
 
 export default useFilter;
